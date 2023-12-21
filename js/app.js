8dc7843747018b84d56b980738029624718a7c33
xTANYAx <nathanambatali07@gmail.com>
2023-12-21 23:42:55 +0800

    Main update

--- js/app.js
+++ js/app.js
@@ -24,3 +24,35 @@ faderightElements.forEach((el) => observer.observe(el));
 
 const fadeinElements = document.querySelectorAll('.fadein');
 fadeinElements.forEach((el) => observer.observe(el));
+
+
+// Toggle Theme
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const about = document.getElementById('aboutme');
+const hero = document.getElementById('theme-hero');
+
+const after = document.querySelector(":root");
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-brightness-high-fill');
+    if(this.classList.toggle('bi-moon')){
+        body.style.background = '#222222';
+        body.style.transition = '1s';
+
+        about.style.background = '#1f1f1f';
+        about.style.color = 'white';
+
+        hero.style.color = 'white';
+        after.style.setProperty("--darkgray", '#222222');
+    }else{
+        body.style.background = 'white';
+        body.style.transition = '1s';
+
+        about.style.background = '#f1f1f1';
+        about.style.color = '#222222';
+
+        hero.style.color = '#222222';
+        after.style.setProperty("--darkgray", 'white');
+    }
+});
